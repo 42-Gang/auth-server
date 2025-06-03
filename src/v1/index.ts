076e33bfd71722effa631630fbbfd31b80073a3f
Jeongsu0880 <latsyrc900@gmail.com>
2025-06-03 21:54:19 +0900

    feat: oauth 라우트 등록

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
 import authRoutes from './apis/auth/auth.route.js';
+import oauthRoutes from './apis/oauth/oauth.route.js';
 
 export default async function routeV1(fastify: FastifyInstance) {
   fastify.register(authRoutes, { prefix: '/auth' });
+  fastify.register(oauthRoutes, { prefix: '/oauth' });
 }
