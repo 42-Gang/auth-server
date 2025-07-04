2547f0e36a239944f3c2fdd2eb21caac59ea63c9
kungbi <kipper12335@gmail.com>
2025-07-04 22:42:09 +0900

    refactor: OTLP exporter로 추적 및 메트릭 내보내기 설정

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,12 +1,20 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
-import { ConsoleMetricExporter, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+    headers: {},
+  }),
+  serviceName: 'auth-service',
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPMetricExporter({
+      url: 'localhost:4318/v1/metrics',
+      headers: {},
+    }),
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
