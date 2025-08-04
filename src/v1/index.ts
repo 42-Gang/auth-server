a04592108e6edb1d6cebec010c05796e5ac1760f
kungbi <kipper12335@gmail.com>
2025-08-04 21:22:19 +0900

    feat: span 이름을 API 요청에 맞게 변경

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -2,8 +2,16 @@ import { FastifyInstance } from 'fastify';
 
 import authRoutes from './apis/auth/auth.route.js';
 import oauthRoutes from './apis/oauth/oauth.route.js';
+import { context, trace } from '@opentelemetry/api';
 
 export default async function routeV1(fastify: FastifyInstance) {
+  fastify.addHook('onRequest', async (request, _) => {
+    const span = trace.getSpan(context.active());
+    if (!span) return;
+
+    span.updateName(`${request.method} ${request.url}`);
+  });
+
   fastify.register(authRoutes, { prefix: '/auth' });
   fastify.register(oauthRoutes, { prefix: '/oauth' });
 }
